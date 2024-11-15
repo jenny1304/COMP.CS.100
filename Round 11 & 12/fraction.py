3936b094694982aa82795f6c43c48586d7940f61
jennypham <jenniepham.143@gmail.com>
2024-11-15 15:13:36 +0200

    Round 11 Final

--- Round 11 & 12/fraction.py
+++ Round 11 & 12/fraction.py
@@ -34,14 +34,6 @@ class Fraction:
         self.__numerator = numerator
         self.__denominator = denominator
 
-    def get_numerator(self):
-        """Get the numerator of the fraction"""
-        return self.__numerator
-
-    def get_denominator(self):
-        """Get the denominator of the fraction"""
-        return self.__denominator
-
     def return_string(self):
         """
         :returns: str, a string-presentation of the fraction in the format
@@ -56,11 +48,24 @@ class Fraction:
 
         return f"{sign}{abs(self.__numerator)}/{abs(self.__denominator)}"
 
+    def __str__(self):
+        """a fraction could be printed just like other datatypes: print(frac)."""
+        if self.__numerator * self.__denominator < 0:
+            sign = "-"
+
+        else:
+            sign = ""
+
+        return f"{sign}{abs(self.__numerator)}/{abs(self.__denominator)}"
+
+
     def simplify(self):
         """Simplify the fraction and return new numerator and denominator"""
         divisor = greatest_common_divisor(self.__numerator,self.__denominator)
-        self.__numerator = self.__numerator//divisor
-        self.__denominator=self.__denominator//divisor
+        numerator = self.__numerator//divisor
+        denominator=self.__denominator//divisor
+        new = Fraction(numerator, denominator)
+        return new
 
     def complement(self):
         """modifying the object that the method is called for"""
@@ -77,32 +82,39 @@ class Fraction:
 
     def multiply(self, frac2):
         """Multiplyy the 2 fractions"""
-        numerator= self.__numerator * frac2.get_numerator()
-        denominator = self.__denominator * frac2.get_denominator()
+        numerator= self.__numerator * frac2.__numerator
+        denominator = self.__denominator * frac2.__denominator
         new = Fraction(numerator,denominator)
         return new
 
     def divide(self,frac2):
         """Divide the 2 fractions"""
-        numerator = self.__numerator * frac2.get_denominator()
-        denominator = self.__denominator * frac2.get_numerator()
+        numerator = self.__numerator * frac2.__denominator
+        denominator = self.__denominator * frac2.__numerator
         new = Fraction(numerator, denominator)
         return new
 
     def add(self, frac2):
         """Add the 2 fractions"""
-        numerator = self.__numerator * frac2.get_denominator() + self.__denominator * frac2.get_numerator()
-        denominator = self.__denominator * frac2.get_denominator()
+        numerator = self.__numerator * frac2.__denominator + self.__denominator * frac2.__numerator
+        denominator = self.__denominator * frac2.__denominator
         new = Fraction(numerator, denominator)
         return new
 
     def deduct(self,frac2):
         """Find the difference between the two fractions"""
-        numerator = self.__numerator * frac2.get_denominator() -frac2.get_numerator() * self.__denominator
-        denominator = self.__denominator * frac2.get_denominator()
+        numerator = self.__numerator * frac2.__denominator-frac2.__numerator * self.__denominator
+        denominator = self.__denominator * frac2.__denominator
         new = Fraction(numerator, denominator)
         return new
 
+    def __lt__(self, other):
+        """If something is less than or equal to other, return true"""
+        return self.__numerator * other.__denominator < other.__numerator * self.__denominator
+
+    def __gt__(self, other):
+        """If something is less than other, return true"""
+        return self.__numerator * other.__denominator > other.__numerator * self.__denominator
 
 def greatest_common_divisor(a, b):
     """
@@ -117,5 +129,25 @@ def greatest_common_divisor(a, b):
 
     return a
 
+def main():
+    list = []
+    print("Enter fractions in the format integer/integer.")
+    print("One fraction per line. Stop by entering an empty line.")
+    while True:
+        fraction = input("")
+        if fraction == "":
+            break
+        else:
+            fraction = fraction.split("/")
+            list.append(Fraction(int(fraction[0]),int(fraction[1])))
+
+
+    print("The given fractions in their simplified form:")
+    for frac in list:
+        print(f"{frac} = {frac.simplify()}")
+
+
+if __name__ == '__main__':
+    main()
 
 
